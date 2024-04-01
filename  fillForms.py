fbe42c6d4f4e53880672cd8560de4b367c63e135
federico Lubbert <federico.lubbert@gmail.com>
2024-04-01 12:36:49 +0200

    init

---  fillForms.py
+++  fillForms.py
@@ -17,22 +17,19 @@ def fill_google_form(data):
         time.sleep(2)  # Adjust based on your internet speed
 
         # Find and fill the address field
-        address_field = driver.find_element(By.XPATH,
-                                            "/html/body/div/div[3]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input")
+        address_field = driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
         address_field.send_keys(entry["address"])
 
         # Find and fill the price field
-        price_field = driver.find_element(By.XPATH,
-                                          "/html/body/div/div[3]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
+        price_field = driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
         price_field.send_keys(entry["price"])
 
         # Find and fill the link field
-        link_field = driver.find_element(By.XPATH,
-                                         "/html/body/div/div[3]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input")
+        link_field = driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
         link_field.send_keys(entry["link"])
 
         # Submit the form
-        submit_button = driver.find_element(By.XPATH, "/html/body/div/div[3]/form/div[2]/div/div[3]/div[1]/div[1]/div")
+        submit_button = driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
         submit_button.click()
 
         # Wait a second before submitting the next form
